e19b1857590649eae35db2b00f663417b525c9fb
Ruslan Manaev <manavrion@yandex.ru>
2018-07-09 15:17:09 +0500

    Add project files.

---
+++ task_manager/task_manager.cpp
@@ -0,0 +1,12 @@
+﻿// task_manager.cpp : Defines the entry point for the application.
+//
+
+#include "task_manager.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
