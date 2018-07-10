bb71acf5ba37d90236929e1c73cdc8848f0d229b
Ruslan Manaev <manavrion@yandex.ru>
2018-07-11 01:10:52 +0500

    Add simple task queue

--- task_manager/task_manager.cpp
+++ task_manager/task_manager.cpp
@@ -4,9 +4,25 @@
 #include "task_manager.h"
 
 using namespace std;
+using namespace task;
 
-int main()
-{
-	cout << "Hello CMake." << endl;
-	return 0;
+int main() {
+  auto lambda = [](int i) -> int {
+    cout << "Run i = " << i << endl;
+    return i + 1;
+  };
+
+  cout << "Test 1" << endl << endl;
+  Task<decltype(lambda), int> task(lambda);
+  task.Run(0);
+
+  cout << "Test 2" << endl << endl;
+  task.Link(lambda);
+  task.Run(0);
+
+  cout << "Test 3 PostTask" << endl << endl;
+  PostTask(lambda, 13);//.Then([](int i) { return i + 1; })
+
+  getchar();
+  return 0;
 }
--- task_manager/task_manager.h
+++ task_manager/task_manager.h
@@ -4,5 +4,63 @@
 #pragma once
 
 #include <iostream>
+#include <memory>
 
 // TODO: Reference additional headers your program requires here.
+
+namespace task {
+
+template <class Arg>
+struct ITask {
+  virtual void Run(Arg arg) = 0;
+};
+
+template <class F, class Arg>
+struct Task : public ITask<Arg> {
+ private:
+  F f;
+
+ public:
+  using argument_t = Arg;
+  using functor_t = F;
+  using result_t = typename std::result_of<F(Arg)>::type;
+
+ private:
+  std::unique_ptr<ITask<result_t>> child;
+
+ public:
+  Task(F f) : f(f) {}
+  void Run(Arg arg) override {
+    auto res = f(std::move(arg));
+    if (child) {
+      child->Run(std::move(res));
+    }
+  }
+
+  template <class G>
+  void Link(G g) {
+    child.reset(new Task<G, result_t>(g));
+  }
+};
+
+template <class F, class Arg>
+struct TaskBuilder {
+  TaskBuilder(Task<F, Arg> task) : task(std::move(task)) {}
+  Task<F, Arg> task;
+
+  //template <class G>
+  //TaskBuilder<Task<G, T::result_t>> Then(G g) {}
+
+  ~TaskBuilder() {
+    task.Run(666);
+  }
+};
+
+template <class F, class Arg>
+decltype(auto) PostTask(F f, Arg arg) {
+  using result = decltype(f(arg));
+  using task_t = Task<F, Arg>;
+  return std::move(TaskBuilder<F, Arg>(task_t(std::move(f))));
+}
+
+}  // namespace task
